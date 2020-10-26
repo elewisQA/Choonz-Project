1a3eb0feb3b449f106ba5af3e6ce5857f089f923
Liam <LHarrold@qa.com>
2020-10-26 12:47:16 +0000

    renamed package to include .choonz

--- src/test/java/com/qa/choonz/persistence/domain/TESTAlbum.java
+++ src/test/java/com/qa/choonz/persistence/domain/TESTAlbum.java
@@ -1,4 +1,4 @@
-package com.qa.persistence.domain;
+package com.qa.choonz.persistence.domain;
 
 //--[ Imports ]--
 import static org.assertj.core.api.Assertions.assertThat;
--- src/test/java/com/qa/choonz/persistence/domain/TESTTrack.java
+++ src/test/java/com/qa/choonz/persistence/domain/TESTTrack.java
@@ -1,4 +1,4 @@
-package com.qa.persistence.domain;
+package com.qa.choonz.persistence.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
