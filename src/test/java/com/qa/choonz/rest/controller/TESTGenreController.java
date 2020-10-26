642ca08d6acf9413c4ae6716a203a417eb3db934
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-26 16:30:21 +0000

    Adjusted import statements.

--- src/test/java/com/qa/choonz/rest/controller/TESTGenreController.java
+++ src/test/java/com/qa/choonz/rest/controller/TESTGenreController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 
