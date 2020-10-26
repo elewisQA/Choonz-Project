205e5ee551e24a264f96a1ac6124053c9a908cde
Nikolett Bajna <NBajna@qa.com>
2020-10-26 15:07:03 +0000

    fixed navbar

--- src/resources/static/js/front.js
+++ src/resources/static/js/front.js
@@ -1,7 +1,7 @@
 $(function() {
   $('#ChangeToggle').click(function() {
     $('#navbar-hamburger').toggleClass('hidden');
-    $('#navbar-close').toggleClass('hidden');  
+    $('#navbar-close').toggleClass('hidden');
   });
 });
 
