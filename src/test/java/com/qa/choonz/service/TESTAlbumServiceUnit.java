9b9ac13dad184ef51f0264bec2cbe839ffd27a2f
AviNissimHart <ahart@qa.com>
2020-10-26 18:04:08 +0000

    I went overtime a bit on the two i worked on. Only issues are with readAll. Will discuss tomorrow before standup

--- src/test/java/com/qa/choonz/service/TESTAlbumServiceUnit.java
+++ src/test/java/com/qa/choonz/service/TESTAlbumServiceUnit.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.qa.choonz.persistence.domain.Album;
 import com.qa.choonz.persistence.domain.Artist;
+import com.qa.choonz.persistence.domain.Genre;
 import com.qa.choonz.persistence.domain.Track;
 import com.qa.choonz.persistence.repository.AlbumRepository;
 import com.qa.choonz.rest.dto.AlbumDTO;
@@ -36,13 +37,15 @@ public class TESTAlbumServiceUnit {
 	private ModelMapper modelMapper;
 	//--[ Test Variables ]--
 		private AlbumDTO albumtDTO;
-		Track testTracks;
+//		Track testTracks;
+		Genre testGenre;
 		Artist testArtist;
 		Album testAlbum;
 		Album testAlbumtWithId;
 		final Long id = 1l;
 		final String name = "Beauty Behind The Madness";
-//		List<Track> testTracks;
+		String cover = "Test";
+		List<Track> testTracks;
 		List<Album> testAlbums;
 
 		//--[ Test Setup ]--
@@ -50,29 +53,73 @@ public class TESTAlbumServiceUnit {
 		void init() {
 			// Initialize testing vars
 			this.testAlbums = new ArrayList<Album>();
-			this.testAlbumtWithId = new Album(this.id, testAlbum.getName(),
-					testAlbum.getTracks(), testAlbum.getArtist(),
-					testAlbum.getGenre(), testAlbum.getCover());
+			this.testAlbum = new Album(this.id, this.name,
+					this.testTracks, this.testArtist,
+					this.testGenre, this.cover);
 
 		}
 		
 		@Test
 		void testCreateAlbum() {
+			when(this.repo.save(this.testAlbum)).thenReturn(this.testAlbumtWithId);
+			
+			when(this.modelMapper.map(this.testAlbumtWithId, AlbumDTO.class)).thenReturn(this.albumtDTO);
+			
+			AlbumDTO expected = this.albumtDTO;
+			AlbumDTO actual = this.service.create(this.testAlbum);
+			assertThat(expected).isEqualTo(actual);
 			
 		}
 		
 		@Test
 		void testReadAlbum() {
+			when(this.repo.findById(this.id)).thenReturn(Optional.of(this.testAlbum));
+			
+			when(this.modelMapper.map(this.testAlbumtWithId, AlbumDTO.class)).thenReturn(this.albumtDTO);
+			
+			assertThat(this.albumtDTO).isEqualTo(this.service.read(this.id));
+			verify(this.repo, times(1)).findById(this.id);
 
 		}
 		
 		@Test
-		void testUpdateAlbum() {
+		void testReadAllAlbums() {
+			when(this.repo.findAll()).thenReturn(this.testAlbums);
+			when(this.modelMapper.map(testAlbumtWithId, AlbumDTO.class)).thenReturn(albumtDTO);
+			
+			assertThat(this.service.read().isEmpty()).isFalse();
+			verify(this.repo, times(1)).findAll();
+			
+		}
 		
+		@Test
+		void testUpdateAlbum() {
+			
+			Album album = new Album(this.id, this.name, this.testTracks, this.testArtist, this.testGenre, this.cover);
+			
+			AlbumDTO albumDTO = new AlbumDTO(this.id, this.name, this.testTracks, this.testArtist, this.testGenre, this.cover);
+			
+			Album updatedAlbum = new Album(this.id, albumDTO.getName(), albumDTO.getTracks(), albumDTO.getArtist(), albumDTO.getGenre(), albumDTO.getCover());
+			
+			AlbumDTO updatedAlbumDTO = new AlbumDTO(this.id, updatedAlbum.getName(), updatedAlbum.getTracks(), updatedAlbum.getArtist(), updatedAlbum.getGenre(), updatedAlbum.getCover());
+			
+			
+			when(this.repo.findById(this.id)).thenReturn(Optional.of(album));
+			when(this.repo.save(album)).thenReturn(updatedAlbum);
+			when(this.modelMapper.map(updatedAlbum, AlbumDTO.class)).thenReturn(updatedAlbumDTO);
+			
+			assertThat(updatedAlbumDTO).isEqualTo(this.service.update(testAlbum, this.id));
+			verify(this.repo, times(1)).findById(1L);
+			verify(this.repo, times(1)).save(updatedAlbum);
 		}
 		
 		@Test
 		void testDeleteAlbum() {
 
+			when(this.repo.existsById(id)).thenReturn(true, false);
+			
+			assertThat(this.service.delete(id)).isFalse();
+			verify(this.repo, times(1)).deleteById(id);
+			verify(this.repo, times(1)).existsById(id);
 		}
 }
