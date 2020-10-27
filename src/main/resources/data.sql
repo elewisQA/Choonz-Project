6cd644cc0370e3e273a84f30e8807af69ec88f0e
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-27 10:48:39 +0000

    Added second album for checking read works.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,7 @@ INSERT INTO GENRE VALUES (2, 'Rock inspired by psychedelic culture', 'Psychedeli
 /* Insert Some Albums */
 INSERT INTO ALBUM(ID, COVER, NAME, ARTIST_ID, GENRE_ID) VALUES (1, '', 'Dark Side of the Moon', 1, 2);
 INSERT INTO ALBUM(ID, COVER, NAME, ARTIST_ID, GENRE_ID) VALUES (2, '', 'Mamma Mia! Official Soundtrack', 2, 1);
+INSERT INTO ALBUM(ID, COVER, NAME, ARTIST_ID, GENRE_ID) VALUES (3, '', 'The Wall', 1, 2);
 /* Add Songs to aforementioned albums */
 INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (1, 1.05, 'Speak To Me', 1);
 INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (2, 2.50, 'Breathe', 1);
@@ -20,6 +21,8 @@ INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (9, 3.47, 'Brain Damage',
 INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (10, 2.10, 'Eclipse', 1);
 INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (11, 3.07, 'Honey, Honey', 2);
 INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (12, 3.35, 'Mamma Mia', 2);
+INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (13, 3.18, 'In the flesh?', 2);
+INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (14, 2.26, 'The Thin Ice', 2);
 /* Add Songs to playlist */
 INSERT INTO PLAYLIST VALUES (1, 'a', 'Number one playlist!', 'Playlist One');
 INSERT INTO PLAYLIST VALUES (2, 'b', 'Second best playlist!', 'Playlist 2');
