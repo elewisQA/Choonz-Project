90fb7b1b23825a07ffd39f64cf30b0f1b5ab6957
Cristina <CLopezCanoyra@qa.com>
2020-10-28 09:49:52 +0000

    Added add album to js file

--- src/resources/static/js/albums.js
+++ src/resources/static/js/albums.js
@@ -37,7 +37,7 @@ fetch('http://localhost:8082/albums/read')
       image.className = "card-img-top";
       image.src = key['cover'];
       card.appendChild(image);
-      let body = document.createElement("body");
+      let body = document.createElement("div");
       body.className = "card-body";
       card.appendChild(body);
 
@@ -55,7 +55,6 @@ fetch('http://localhost:8082/albums/read')
       linkArtist.href = "#";
       let artist = document.createElement("p");
       artist.className = "card-text";
-      artist.style = "text-align: center"
       let artistSmall = document.createElement("small");
       artistSmall.className = "text-muted";
       let artistSmallText = document.createTextNode(key['artistName']);
@@ -65,4 +64,31 @@ fetch('http://localhost:8082/albums/read')
       artistSmall.appendChild(artistSmallText);
       
     }
+    let find = document.getElementById("albums");
+    let column = document.createElement("div");
+    column.className = "col mb-4";
+    let card = document.createElement("div");
+    card.className = "card text-center";
+    find.appendChild(column);
+    column.appendChild(card);
+
+    let image = document.createElement("img");
+    image.className = "card-img-top";
+    image.src = "https://i.pinimg.com/originals/c9/db/b1/c9dbb1ad7c558a37c6291681aca99058.jpg";
+    card.appendChild(image);
+
+    let body = document.createElement("div");
+    body.className = "card-body";
+    card.appendChild(body);
+
+    let linkAdd = document.createElement("a");
+    linkAdd.className = "btn btn-info";
+    linkAdd.href ="#";
+    body.appendChild(linkAdd);
+
+    let icon = document.createElement("i");
+    icon.className = "fas fa-plus";
+    icon.style = "color: whitesmoke;";
+    linkAdd.appendChild(icon);
+
   }
