4a2b49c7a4069acf86d1a0a854a7e0357f7dd85b
Cristina <CLopezCanoyra@qa.com>
2020-10-30 17:51:28 +0000

    Added the playlist page

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 /* Insert Some Artists */
 INSERT INTO ARTIST(ID, PICTURE, NAME) VALUES (1, 'https://cdn.britannica.com/64/23164-050-A7D2E9D9/Pink-Floyd.jpg', 'Pink Floyd');
 INSERT INTO ARTIST(ID, PICTURE, NAME) VALUES (2, 'https://i.pinimg.com/736x/f9/8b/c0/f98bc0f9c2ed2ec01816665ccc0a9d1a.jpg', 'ABBA');
-INSERT INTO ARTIST(ID, PICTURE, NAME) VALUES (3,'https://i.pinimg.com/originals/81/bd/9f/81bd9f9ecfbdcba163154073ab44eaaa.jpg' ,'Gorrilaz');
+INSERT INTO ARTIST(ID, PICTURE, NAME) VALUES (3,'https://i.pinimg.com/originals/81/bd/9f/81bd9f9ecfbdcba163154073ab44eaaa.jpg' ,'Gorillaz');
 /* Insert some Genres */
 INSERT INTO GENRE(ID, PICTURE, DESCRIPTION, NAME) VALUES (1, 'https://www.udiscovermusic.com/wp-content/uploads/2017/04/1000x600oktolikediscoresize-web-optimised.jpg', 'Time to Dance!', 'Disco');
 INSERT INTO GENRE(ID, PICTURE, DESCRIPTION, NAME) VALUES (2, 'https://m.media-amazon.com/images/I/71i0QeO0IOL._SS500_.jpg', 'Rock inspired by psychedelic culture', 'Psychedelic Rock');
@@ -23,8 +23,8 @@ INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (10, 2.10, 'Eclipse', 1);
 INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (11, 3.07, 'Honey, Honey', 2);
 INSERT INTO TRACK(ID, DURATION, NAME, ALBUM_ID) VALUES (12, 3.35, 'Mamma Mia', 2);
 /* Insert some playlists */
-INSERT INTO PLAYLIST VALUES (1, 'a', 'Number one playlist!', 'Playlist One');
-INSERT INTO PLAYLIST VALUES (2, 'b', 'Second best playlist!', 'Playlist 2');
+INSERT INTO PLAYLIST VALUES (1, 'https://i.pinimg.com/564x/38/41/48/384148d27fb01b2ff9455e05aa7b3557.jpg', 'Number one playlist!', 'Playlist One');
+INSERT INTO PLAYLIST VALUES (2, 'https://i.pinimg.com/564x/d0/5b/98/d05b98026f6628b5f23c33c65a3d36b5.jpg', 'Second best playlist!', 'Playlist 2');
 /* Add some songs to a playlist */ 
 INSERT INTO track_playlists VALUES(1, 1);
 INSERT INTO track_playlists VALUES(11, 1);
