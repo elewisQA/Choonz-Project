9ffd1369d2e8502be5931d9d87661a018bbb0940
Nikolett Bajna <NBajna@qa.com>
2020-10-30 09:06:09 +0000

    updated artist create

--- src/resources/static/js/newArtist.js
+++ src/resources/static/js/newArtist.js
@@ -1,22 +1,28 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-// (function() {
-//     'use strict';
-//     window.addEventListener('load', function() {
-//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//       var forms = document.getElementsByClassName('needs-validation');
-//       // Loop over them and prevent submission
-//       var validation = Array.prototype.filter.call(forms, function(form) {
-//         form.addEventListener('submit', function(event) {
-//           if (form.checkValidity() === false) {
-//             event.preventDefault();
-//             event.stopPropagation();
-//           }
-//             form.classList.add('was-validated');
-//         }, false);
-//       });
-//     }, false);
-//   })();
-
+    function validate(){
+        stop.preventDefault();
+        (function() {
+          'use strict';
+          window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+              form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                  validation.setCustomValidity = "Enter all the details";
+                }else{
+                    form.classList.add('was-validated');
+                    validation.setCustomValidity = "valid";
+                }
+              }, false);
+            });
+          }, false);
+        })();
+    }
+    
+    
     document.querySelector("form#createNewArtist").addEventListener("submit", function(stop){
         stop.preventDefault();
         (function() {
@@ -30,16 +36,19 @@
                 if (form.checkValidity() === false) {
                   event.preventDefault();
                   event.stopPropagation();
+                  validation.setCustomValidity = "Enter all the details";
+                }else{
+                    form.classList.add('was-validated');
+                    validation.setCustomValidity = "valid";
                 }
-                  form.classList.add('was-validated');
               }, false);
             });
           }, false);
         })();
         let artistModal = document.querySelector("form#createNewArtist").elements;
         console.log(artistModal);
-        let artistName = artistModal['artistName'].value;
-        let artistPic = artistModal['artistPic'].value;
+        let artistName = artistModal['name'].value;
+        let artistPic = artistModal['pic'].value;
         addArtist(artistName, artistPic);
         
     });
@@ -60,7 +69,7 @@
             .then(json)
             .then(function (data) {
                 console.log('Request succeeded with JSON response', data);
-                location.reload();
+                // location.reload();
             })
             .catch(function (error) {
                 console.log('Request failed', error);
