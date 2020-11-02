d3ed4bd4b75ed6d39490cf8ab6d306122b449315
Elizabeth Lewis <elewis@academytrainee.com>
2020-11-02 12:01:01 +0000

    Changed static-typing.

--- src/main/java/com/qa/choonz/utils/AuthUtils.java
+++ src/main/java/com/qa/choonz/utils/AuthUtils.java
@@ -7,36 +7,38 @@ import java.util.Random;
 import com.qa.choonz.exception.TokenNotFoundException;
 
 public class AuthUtils {
-	private final int tokenLength = 10;
+	private static final int tokenLength = 10;
 	private static Map<String, Long> userTokens;
 	private static Map<String, Long> adminTokens; // Future-proofing, used to lock-off API calls
 
 	public AuthUtils() {
-		this.userTokens = new HashMap<>();
+		if (AuthUtils.userTokens != null) {
+			AuthUtils.userTokens = new HashMap<>();
+		}
 	}
 	
 	//--[ Login Methods ]--
-	public String newToken(Long userId) {
+	public static String newToken(Long userId) {
 		String token = generateToken();
-		userTokens.put(token, userId);
+		AuthUtils.userTokens.put(token, userId);
 		return token;
 	}
 	
-	public Long getTokenOwner(String token) throws TokenNotFoundException {
-		if (userTokens.containsKey(token)) {
-			return userTokens.get(token);
+	public static Long getTokenOwner(String token) throws TokenNotFoundException {
+		if (AuthUtils.userTokens.containsKey(token)) {
+			return AuthUtils.userTokens.get(token);
 		} else {
 			throw new TokenNotFoundException();
 		}
 	}
 	
-	public void deleteToken(String token) {
-		if (userTokens.containsKey(token)) {
-			userTokens.remove(token);
+	public static void deleteToken(String token) {
+		if (AuthUtils.userTokens.containsKey(token)) {
+			AuthUtils.userTokens.remove(token);
 		}
 	}
 	
-	private String generateToken() {
+	private static String generateToken() {
 		// Use random-number generated values, cast to chars.
 		int lowerLimit = 48;	// Set bounds for character values, lowest is '0', highest is 'z'
 		int upperLimit = 122;
@@ -44,7 +46,7 @@ public class AuthUtils {
 		// Thanks to Baeldung for this code
 		return rng.ints(lowerLimit, upperLimit + 1)
 	    	      .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))	// Filter-method to avoid going out of range
-	    	      .limit(this.tokenLength)
+	    	      .limit(AuthUtils.tokenLength)
 	    	      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
 	    	      .toString();
 	}
