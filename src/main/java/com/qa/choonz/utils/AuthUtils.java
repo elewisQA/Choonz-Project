308e04f3ab5ce18bdf1e326c1dbfc38a53f97f61
Elizabeth Lewis <elewis@academytrainee.com>
2020-11-02 11:48:23 +0000

    Added authentication-token manager.

---
+++ src/main/java/com/qa/choonz/utils/AuthUtils.java
@@ -0,0 +1,45 @@
+package com.qa.choonz.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import com.qa.choonz.exception.TokenNotFoundException;
+
+public class AuthUtils {
+	private final int tokenLength = 10;
+	private static Map<String, Long> userTokens;
+	private static Map<String, Long> adminTokens; // Future-proofing, used to lock-off API calls
+
+	public AuthUtils() {
+		this.userTokens = new HashMap<>();
+	}
+	
+	//--[ Login Methods ]--
+	public String newToken(Long userId) {
+		String token = generateToken();
+		userTokens.put(token, userId);
+		return token;
+	}
+	
+	public Long getTokenOwner(String token) throws TokenNotFoundException {
+		if (userTokens.containsKey(token)) {
+			return userTokens.get(token);
+		} else {
+			throw new TokenNotFoundException();
+		}
+	}
+	
+	private String generateToken() {
+		// Use random-number generated values, cast to chars.
+		int lowerLimit = 48;	// Set bounds for character values, lowest is '0', highest is 'z'
+		int upperLimit = 122;
+		Random rng = new Random();
+		// Thanks to Baeldung for this code
+		return rng.ints(lowerLimit, upperLimit + 1)
+	    	      .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))	// Filter-method to avoid going out of range
+	    	      .limit(this.tokenLength)
+	    	      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+	    	      .toString();
+	}
+}
