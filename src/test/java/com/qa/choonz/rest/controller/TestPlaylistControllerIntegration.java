f435f7ff7b80ed74f625b6abd42622bf124256f0
Liam <LHarrold@qa.com>
2020-11-03 12:54:28 +0000

    created add track integration test

--- src/test/java/com/qa/choonz/rest/controller/TestPlaylistControllerIntegration.java
+++ src/test/java/com/qa/choonz/rest/controller/TestPlaylistControllerIntegration.java
@@ -157,4 +157,20 @@ class TestPlaylistControllerIntegration {
         this.mock.perform(request(HttpMethod.DELETE, "/playlists/delete/" + this.id).header("token", token)).andExpect(status().isNoContent());
     }
     
+    @Test
+    void testAddTrack() throws Exception{
+    	Track track = this.trackRepo.findById(2L).orElseThrow(TrackNotFoundException::new);
+    	List<Track> trackList = new ArrayList<>();
+    	trackList.add(track);
+    	testPlaylist.setTracks(trackList);
+    	
+        String output = this.mock
+                .perform(request(HttpMethod.POST, "/playlists/update/" + this.id).header("token", token).accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.objectMapper.writeValueAsString(testPlaylist)))
+                .andExpect(status().isAccepted()).andReturn().getResponse().getContentAsString();
+    	
+    	assertEquals(this.objectMapper.writeValueAsString(this.mapToDTO(testPlaylist)),output);
+    }
+    
 }
