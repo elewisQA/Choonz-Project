275b601c0ddad59398813418c11f7889898538bb
AviNissimHart <ahart@qa.com>
2020-11-04 14:22:08 +0000

    Adjusted package structure

--- src/test/java/com/qa/choonz/cuke/stepdefs/testArtistsSelinium.java
+++ src/test/java/com/qa/choonz/cuke/stepdefs/testArtistsSelinium.java
@@ -1,11 +1,19 @@
-package com.qa.choonz.selinium;
+package com.qa.choonz.cuke.stepdefs;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class testArtistsSelinium {
 
@@ -21,8 +29,14 @@ public class testArtistsSelinium {
 	@Given("^the correct web address$")
 	public void the_correct_web_address() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
-		driver.get("http://www.practiceselenium.com/welcome.html");
+		driver.get("http://127.0.0.1:5501/static/index.html");
 		
-		System.out.println("tea website accessed");
+		System.out.println("choonz website accessed");
+	}
+	
+	@After
+	public void tearDown() {
+		driver.close();
+		driver.quit();
 	}
 }
