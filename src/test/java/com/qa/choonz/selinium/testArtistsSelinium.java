4883083b425002286ada82d0626618f1cfe94216
AviNissimHart <ahart@qa.com>
2020-11-04 14:02:15 +0000

    sorted gitignore for driver

--- src/test/java/com/qa/choonz/selinium/testArtistsSelinium.java
+++ src/test/java/com/qa/choonz/selinium/testArtistsSelinium.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
 
 public class testArtistsSelinium {
 
@@ -17,4 +18,11 @@ public class testArtistsSelinium {
         driver = new ChromeDriver();
 	}
 	
+	@Given("^the correct web address$")
+	public void the_correct_web_address() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		driver.get("http://www.practiceselenium.com/welcome.html");
+		
+		System.out.println("tea website accessed");
+	}
 }
--- src/test/resources/cuke/testArtistsSelinium.feature
+++ src/test/resources/cuke/testArtistsSelinium.feature
@@ -4,7 +4,7 @@ I want to browse artists on the website
 So i can add to playlist
 
   Scenario: Browse artists on website to add song to playlist
-    Given The correct web address
+    Given the correct web address
     When I navigate to the 'Artist' page
     And I can click on an artist
     Then I can select an album from the artist
