d74d7c401da47330a02b9991a2d8e70c268adb49
Elizabeth Lewis <elewis@academytrainee.com>
2020-11-06 11:48:15 +0000

    Tried getting choonz-logo working.

--- src/main/java/com/qa/choonz/rest/controller/RouteContoller.java
+++ src/main/java/com/qa/choonz/rest/controller/RouteContoller.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-//@RequestMapping("/site")
 public class RouteContoller {
     @GetMapping(value = "/")
     public String index() {
