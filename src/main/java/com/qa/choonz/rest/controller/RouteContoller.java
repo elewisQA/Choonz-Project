7c0da0607e41dbae9693c9c0032df9c7cc395ecc
Elizabeth Lewis <elewis@academytrainee.com>
2020-11-06 11:44:05 +0000

    Removed '../static/' references from web-pages.

--- src/main/java/com/qa/choonz/rest/controller/RouteContoller.java
+++ src/main/java/com/qa/choonz/rest/controller/RouteContoller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/site")
+//@RequestMapping("/site")
 public class RouteContoller {
     @GetMapping(value = "/")
     public String index() {
